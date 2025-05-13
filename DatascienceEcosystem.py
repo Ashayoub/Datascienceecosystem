742a54b6180b519463a7b3ae2650bfec6d5993b2
Ashayoub <habach.ayoub.ah@gmail.com>
2025-05-13 13:48:06 +0100

    Datascienceecosystem

---
+++ DatascienceEcosystem.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# 
+# - Convert minutes to hours using a simple mathematical operation.
+# - Understand how to use Python for basic unit conversions.
+# - Learn how to use comments in code to explain operations.
+# - Explore how to use markdown for clear and concise documentation.
+# - Understand the process of working with code cells and markdown cells in Jupyter notebooks.
+
+# Some of the popular languages that Data Scientist use are : 
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Pandas
+# 2. Numpy
+# 3. Tensorflow
+# 4. Matplotlib
+#    
+
+# | Data Science Tools       |
+# |--------------------------|
+# |Jupyter Notebook          |
+# | R Studio                 |
+# | Vs Code                  |
+# 
+# 
+# 
+# 
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[3]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[5]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+minutes = 200
+hours = minutes /60
+hours
+
+
+# ## Author
+# 
+# HABACH AYOUB
+
+# In[ ]:
+
+
+
+
